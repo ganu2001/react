34bb2b54b615c12af04e10e9b540af128149311a
ganu2001 <ganeshphutane2001@gmail.com>
2024-09-03 15:01:50 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+
+  let [counter , setcounter] = useState(0)
+
+  const addvale = () =>{
+    counter=counter +1
+    setcounter(counter)
+  }
+
+  const Removevale = () =>{
+    if(counter > 0){
+      counter=counter -1
+    }
+    setcounter(counter)
+  }
+
+
+  return (
+    <>
+      <h2>Add or Remove Value on click button</h2>
+      <div>
+        <h1>Counter Value {counter}</h1>
+      </div>
+      <button onClick={addvale}  >Addvalue</button>
+      
+      <button onClick={Removevale}  >Removevalue</button>
+    </>
+  )
+}
+
+export default App
